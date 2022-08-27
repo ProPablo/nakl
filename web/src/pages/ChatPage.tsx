b9abfdd4e421e171eb75405411e68664d3779ca5
anhad <anhadrs@gmail.com>
2022-08-28 05:20:51 +1000

    Working chat

--- web/src/pages/ChatPage.tsx
+++ web/src/pages/ChatPage.tsx
@@ -1,45 +1,69 @@
+//@ts-ignore
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { ChatContainer, MainContainer, Message, MessageInput, MessageList, MessageModel } from "@chatscope/chat-ui-kit-react";
-import { useState } from "react";
+import { Ref, useRef, useState } from "react";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { CurrentConnectionContext, SocketContext } from "../App";
-import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 
 const ChatPage = () => {
 
+  const inputRef = useRef<HTMLDivElement>(null);
+  const [msgInputValue, setMsgInputValue] = useState("");
+
   const [messages, setMessages] = useState<MessageModel[]>([]);
-  const conn = useContext(CurrentConnectionContext);
+  const connRef = useContext(CurrentConnectionContext);
   const navigate = useNavigate();
   useEffect(() => {
-    if (conn.current == null) {
+    if (connRef.current == null) {
       navigate("/");
       return;
     }
 
-    conn.current.on("data", (data) => {
+    connRef.current.on("data", (data) => {
       console.log("data recieved ", data);
-      setMessages(existing => {
-        if (typeof (data) == "string") {
+      if (typeof (data) == "string") {
+        setMessages(existing => {
           const newModel: MessageModel = {
             direction: "incoming",
             position: "single",
             message: data
           }
-          return [...existing,]
-        }
-        else if (data instanceof Blob) {
+          return [...existing, newModel];
+        });
+      }
+      else if (data instanceof Blob) {
 
-        }
-      })
-    });
+      }
+    })
+
 
     return () => {
-      // conn.current.removeAllListeners();
-      // conn.current.close();
+      connRef.current.removeAllListeners();
+      connRef.current.close();
     }
-
   }, [])
-  //On disconnect go to root, you will get a new peer
+
+
+
+
+  const handleSend = (message) => {
+
+    connRef.current.send(message);
+    setMessages([...messages,
+    {
+      position: 'single',
+      message,
+      direction: 'outgoing'
+    }
+    ]);
+    setMsgInputValue("");
+    inputRef.current.focus();
+
+  }
+
+
+
   return (<div style={{
     height: "90vh",
     width: "100vw"
@@ -55,13 +79,17 @@ const ChatPage = () => {
             />
           )}
 
-          <MessageInput placeholder="Type message here" />
-
         </MessageList>
+        <MessageInput
+          placeholder="Type message here"
+          onSend={handleSend}
+          onChange={setMsgInputValue}
+          value={msgInputValue}
+          ref={inputRef}
+        />
       </ChatContainer>
     </MainContainer>
-    IN CHAT</div>)
-
+  </div>)
 }
 
 export default ChatPage;
