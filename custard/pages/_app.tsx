78c9bf50e910b466f92554139f3e92e9dec305dc
sh1ggy <tyronewessnolasco@gmail.com>
2023-03-08 19:50:08 +1000

    peerJS gen & contexts kinda working

--- custard/pages/_app.tsx
+++ custard/pages/_app.tsx
@@ -1,38 +1,66 @@
 import type { AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-
-import { Component, useEffect, useRef, useState } from 'react';
-
+import React, { createContext, MutableRefObject, useEffect, useRef, useState } from 'react';
 import '@/styles/globals.css'
+import type { Peer, DataConnection } from "peerjs"
 
+export const GlobalContext = createContext(null);
+export const SocketContext = createContext(null);
+export const CurrentConnectionContext = createContext(null);
+
+
+// Defining global state interfaces and default values
+export interface GlobalState {
+  peerId: string, 
+  isLoadingPeer: boolean,
+}
+
+const initialState: GlobalState = {
+  peerId: '', 
+  isLoadingPeer: true,
+}
+
+
+
+// Defining constants to be used for state
+const FALLBACK_PAGE = "fallback";
+const MONIKER_PAGE = "pc"; 
+const HomePages = [FALLBACK_PAGE, MONIKER_PAGE]
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter(); // for navigation
-  const [peerId, setPeerId] = useState("");
-
-  const connRef = useRef<any>(null);
-
+  let Peer, DataConnection;
   const importPeer = async () => {
-    const Peer = (await import('peerjs')).default
-    const peer = new Peer();
-
-    peer.on("open", (id: any) => {
-      setPeerId(id);
-    })
-
-    //This is when a peer connects to us
-    peer.on("connection", (conn: any) => {
-      connRef.current = conn;
-      router.push("/chat");
-    })
+    Peer = (await import('peerjs')).default
+    DataConnection = (await import('peerjs')).default
   }
+
+
+  const peerRef = useRef<typeof Peer>(null);
+  const connRef = useRef<typeof DataConnection | null>(null);
+
+  const stateAndDispatch = useState(initialState);
+  const router = useRouter(); // for navigation
+  
+
+  // const [peerId, setPeerId] = useState("");
+
   useEffect(() => {
     importPeer();
-
+    if (peerRef.current == null && !HomePages.some(h => h == router.pathname)) {
+      console.log("Going home.");
+      router.push("/")
+    }
     console.log("Making new peer");
 
   }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <GlobalContext.Provider value={stateAndDispatch}>
+      <SocketContext.Provider value={peerRef}>
+        <CurrentConnectionContext.Provider value={connRef}>
+          <Component {...pageProps} />    
+        </CurrentConnectionContext.Provider>
+      </SocketContext.Provider>
+    </GlobalContext.Provider>
+  ) 
 }
--- custard/pages/index.tsx
+++ custard/pages/index.tsx
@@ -2,12 +2,50 @@ import Head from 'next/head'
 
 import styles from '@/styles/Home.module.css'
 import QRCode from "react-qr-code";
+import { useContext, useEffect } from 'react';
+import { CurrentConnectionContext, GlobalContext, SocketContext } from './_app';
+import { useRouter } from 'next/router';
 
 // Constants
 const PEER_SERVER = 'peer.kongroo.xyz';
 
 
 export default function Home() {
+  const peer = useContext(SocketContext);
+  const [state, setGlobalState] = useContext(GlobalContext);
+  const connRef = useContext(CurrentConnectionContext);
+  const router = useRouter();
+  let Peer, DataConnection;
+
+  const importPeer = async () => {
+    Peer = (await import('peerjs')).default
+    DataConnection = (await import('peerjs')).default
+    peer.current = new Peer(); //fallback for now 
+    setGlobalState({
+      ...state, 
+      isLoadingPeer: true,
+      peerId: "",
+    })
+
+    console.log("Making new peer.");
+    peer.current.on("open", (id) => {
+      setGlobalState({
+        ...state,
+        isLoadingPeer: false,
+        peerId: id,
+      })
+    })
+
+    // when peer connects to us
+    peer.current.on("connection", (conn) => {
+      connRef.current = conn;
+      console.log("Someone decided to join.");
+      router.push("/chat");
+    })
+  }
+  useEffect(() => {
+    importPeer();
+  }, [])
 
   return (
     <>
@@ -20,24 +58,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className="flex flex-col justify-center items-center h-screen">
-
           <img className="w-16 md:w-32 lg:w-48" src='/custard.svg' />
           <h1 className="text-3xl font-bold underline text-center">
             Custard
           </h1>
-          <button className="btn btn-primary">Join</button>
-          <QRCode
-            className="qr-code justify-centre" value={"custard"} />
-          {/* value={state.peerId} /> */}
-
-          {/* 
-          <code>{state.peerId}</code>
-
-          <Link to="/join">
-          </Link> */}
-
-          {/* {state.isLoadingPeer ? <div> Loading </div> : */}
-          {/* } */}
+          {state.isLoadingPeer ? 
+            <div>
+              <h1 className="text-3xl font-bold underline text-center">LOADING</h1>
+            </div>
+            :
+            <div>
+              <QRCode className="qr-code justify-centre" value={state.peerId} />
+              <code>{state.peerId}</code>
+              <button className="btn btn-primary">Join</button>
+            </div>
+          }
         </div>
       </main>
     </>
