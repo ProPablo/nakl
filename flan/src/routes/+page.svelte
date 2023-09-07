44312bc17c498d4d4525067a023d9ef81a6ef123
sh1ggy <tyronewessnolasco@gmail.com>
2023-09-07 19:59:24 +1000

    init sveltekit skeleton

---
+++ flan/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+	import { AppBar, FileDropzone } from "@skeletonlabs/skeleton";
+
+</script>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">hello</svelte:fragment>
+	(title)
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">hel</h1>
+		<FileDropzone name="files" />
+	</div>
+</div>
