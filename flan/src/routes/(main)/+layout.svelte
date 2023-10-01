f88af73c65df0d85f883576fbd46e00576d46ee6
sh1ggy <tyronewessnolasco@gmail.com>
2023-10-01 15:01:56 +1000

    Placeholder for connect page

--- flan/src/routes/(main)/+layout.svelte
+++ flan/src/routes/(main)/+layout.svelte
@@ -49,10 +49,12 @@
 
 	<div class="h-full">
 		{#if loadingPeer}
-			<div class="flex justify-center items-center h-full">
-				<p class="bg-secondary-500 p-3 rounded-lg animate-pulse">Loading & connecting to peer...</p>
+			<div class="flex flex-col items-center h-full gap-3 pt-10">
+				<p class="badge-glass p-3 rounded-lg animate-pulse">Loading & connecting to peer...</p>
+				<div class="placeholder animate-pulse rounded-lg pb-[315px] w-[315px]"></div>
+				<p class="placeholder p-3 rounded-lg animate-pulse w-1/4"></p>
 			</div>
-		{:else}
+			{:else}
 			<slot />
 		{/if}
 	</div>
