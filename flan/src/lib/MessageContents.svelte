c94b4cd1b1812e3fe642e71a6596f2181dd34379
sh1ggy <tyronewessnolasco@gmail.com>
2023-10-01 13:33:55 +1000

    Rendering audio & video

--- flan/src/lib/MessageContents.svelte
+++ flan/src/lib/MessageContents.svelte
@@ -8,10 +8,19 @@
 	<p>{message.text}</p>
 {:else if message.type === MessageType.Image}
 	<div class="flex items-center justify-center">
-		<img
-			class="rounded-lg max-h-[60vh]"
-			src={message.payload?.src}
-			alt={message.payload?.name} />
+		<img class="rounded-lg max-h-[60vh]" src={message.payload?.src} alt={message.payload?.name} />
+	</div>
+{:else if message.type === MessageType.Audio}
+	<div class="flex flex-col gap-3 items-center justify-center">
+		<audio controls src={message.payload?.src} />
+		<code>{message.payload?.name}</code>
+	</div>
+{:else if message.type === MessageType.Video}
+	<div class="flex flex-col gap-3 items-center justify-center">
+		<video controls src={message.payload?.src} class="rounded-lg">
+			<track kind="captions" />
+		</video>
+		<code>{message.payload?.name}</code>
 	</div>
 {:else if message.type === MessageType.File}
 	<div class="flex items-center justify-center">
