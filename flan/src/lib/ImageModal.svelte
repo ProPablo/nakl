0e70e62cdf52140d3a332fb60b23ce6aa27768ab
sh1ggy <tyronewessnolasco@gmail.com>
2023-10-11 17:06:01 +1000

    THIS IS CHAT GPT CODE for image zoom

--- flan/src/lib/ImageModal.svelte
+++ flan/src/lib/ImageModal.svelte
@@ -2,26 +2,45 @@
 	import { getModalStore } from '@skeletonlabs/skeleton';
 
 	export let parent: any;
-
 	const modalStore = getModalStore();
+
+	let isZoomed = false;
+	let mouseX = 0;
+	let mouseY = 0;
+
+	function handleMouseMove(event: MouseEvent) {
+		if (!isZoomed || !event) return;
+		const target = event.target as HTMLElement;
+		const rect = target.getBoundingClientRect();
+		mouseX = (event.clientX - rect.left) / rect.width;
+		mouseY = (event.clientY - rect.top) / rect.height;
+	}
 </script>
 
 {#if $modalStore[0]}
-	<div class="flex items-center justify-center">
+	<div class="flex flex-col gap-4 items-center justify-center">
 		<button
 			on:click={parent.onClose}
 			class="btn-icon variant-filled fixed top-4 right-4 z-50 font-bold shadow-xl">
 			x
 		</button>
-		<img
-			on:contextmenu={(e) => {
-				e.preventDefault();
-				e.stopPropagation();
-				return false;
-			}}
-			class="rounded-lg max-h-[80vh]"
-			src={$modalStore[0].meta?.image}
-			alt={$modalStore[0].meta?.name} />
+		<button on:click={() => (isZoomed = !isZoomed)}>
+			<img
+				on:mousemove={handleMouseMove}
+				on:contextmenu={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					return false;
+				}}
+				style="transform-origin: {mouseX * 100}% {mouseY * 100}%;"
+				class={`rounded-lg max-h-[80vh] transition-transform transform-gpu 
+				${isZoomed && 'scale-125'}`}
+				src={$modalStore[0].meta?.image}
+				alt={$modalStore[0].meta?.name} />
+		</button>
+		{#if !isZoomed}
+			<p class="badge-glass p-3 rounded-lg">Click Image to Zoom</p>
+		{/if}
 	</div>
 {/if}
 
